82d07fe71b7ae8f99627d2c7e6ba60ba2044f7bc
aleksejmoksancev <mokshantsev.aleksei>
2023-07-20 01:29:23 +0500

    fix

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,7 @@ set -e
 IMAGE="tgf:latest"
 CONTAINER_ID=`docker ps -aqf "name=tgf"`
 
-echo "dsahejqhjehq hdhsaehq ${TWITCH_CLIENT_ID}"
-
 docker stop $CONTAINER_ID
 docker rm $CONTAINER_ID
 
 docker build -t $IMAGE .
-
-docker run --restart unless-stopped -d -p 80:8080 --name tgf -e TWITCH_CLIENT_ID="${TWITCH_CLIENT_ID}" -e TWITCH_CLIENT_SECRET="${TWITCH_CLIENT_SECRET}" $IMAGE
-docker system prune -a -f
