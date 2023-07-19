fc9d72323c1a20b096471481aa25cd68dc34de50
aleksejmoksancev <mokshantsev.aleksei>
2023-07-19 23:47:15 +0500

    add deploy.sh and workflow

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+IMAGE="tgf:latest"
+CONTAINER_ID=docker ps -aqf "name=tgf"
+
+docker build -t IMAGE .
+
+docker stop ${CONTAINER_ID}
+docker run --restart unless-stopped -d -p 80:8080 --name tgf -e TWITCH_CLIENT_ID=${TWITCH_CLIENT_ID} -e TWITCH_CLIENT_SECRET=${TWITCH_CLIENT_SECRET} ${IMAGE}
+docker system prune -a -f
