ef07a723b46ed1a1f2c725152f25b5e41b7b95f4
aleksejmoksancev <mokshantsev.aleksei>
2023-07-20 17:42:56 +0500

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 set -e
 
 IMAGE="tgf:latest"
-CONTAINER_ID=`docker ps -aqf "name=tgf"`
 
-docker stop $CONTAINER_ID
-docker rm $CONTAINER_ID
+docker stop tgf
+docker rm tgf
 
 docker build -t $IMAGE .
